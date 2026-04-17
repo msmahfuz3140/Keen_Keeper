0dc4b1f01ac813a3145d44993a509b3e1fe9137f
msmahfuz3140 <mdmahfuzulhaque3140@gmail.com>
2026-04-17 17:19:56 +0600

    added looding spinner

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Banner from './Home-componensts/Banner';
 import Count from './Home-componensts/Count';
 import AllFriendsFatch from '../../AllFriends/AllFriendsFatch';
@@ -8,8 +8,20 @@ const Home = () => {
         <div className='max-w-[1200px] mx-auto'>
             <Banner></Banner>
             <Count></Count>
-              <div className="divider mt-4"></div>
-              <AllFriendsFatch></AllFriendsFatch>
+            <div className="divider mt-4"></div>
+            <Suspense fallback={<div className="flex flex-col items-center justify-center min-h-screen w-full gap-4">
+
+                {/* Spinner */}
+                <span className="loading loading-spinner loading-lg text-primary scale-150"></span>
+
+                {/* Loading text */}
+                <p className="text-sm text-gray-500 animate-pulse">
+                    Loading friends...
+                </p>
+
+            </div>}>
+                <AllFriendsFatch></AllFriendsFatch>
+            </Suspense>
         </div>
     );
 };
